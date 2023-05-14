b75f3c446d0fbc441be73d3138594cb889965f13
Lisiasty <sonicmephit@gmail.com>
2023-05-14 11:26:53 +0200

    Player UI overlay

--- Assets/Scripts/Player/PlayerGameUI.cs
+++ Assets/Scripts/Player/PlayerGameUI.cs
@@ -7,21 +7,21 @@ public class PlayerGameUI : MonoBehaviour
 {
     public Camera pCamera;
     private Canvas gameUICanvas;
-    public Text playerName;
+    [SerializeField] private Text playerName;
+    
     
     public void Start() 
     {
         pCamera = Camera.main;
         gameUICanvas = GetComponent<Canvas>();
         gameUICanvas.worldCamera = pCamera;
+        playerName.text = GetComponentInParent<PlayerController>().playerName;
     }
 
     private void Update() 
     {
         transform.LookAt(transform.position + pCamera.transform.rotation * Vector3.forward,
         pCamera.transform.rotation * Vector3.up);
-
-        
     }
 
 }
