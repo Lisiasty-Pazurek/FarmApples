ad949df85f32a9d54074fd47410a9a0c50442419
Lisiasty <sonicmephit@gmail.com>
2023-05-14 12:15:37 +0200

    new nameplate

--- Assets/Scripts/Player/PlayerGameUI.cs
+++ Assets/Scripts/Player/PlayerGameUI.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using MirrorBasics;
 
 public class PlayerGameUI : MonoBehaviour
 {
     public Camera pCamera;
     private Canvas gameUICanvas;
     [SerializeField] private Text playerName;
+    [SerializeField] private Image namePlate;
     
     
     public void Start() 
@@ -16,6 +18,21 @@ public class PlayerGameUI : MonoBehaviour
         gameUICanvas = GetComponent<Canvas>();
         gameUICanvas.worldCamera = pCamera;
         playerName.text = GetComponentInParent<PlayerController>().playerName;
+        SetTeam();
+    }
+
+    public void SetTeam()
+    {
+        if (GetComponentInParent<PlayerScore>().teamID == 1)
+        {
+            namePlate.color = new Color( .5f, .8f, 1, 1);
+            print("color 1");
+        }
+        if (GetComponentInParent<PlayerScore>().teamID == 2)
+        {
+            namePlate.color = new Color(1, .5f, .5f, 1);
+            print("color 2");
+        }        
     }
 
     private void Update() 
